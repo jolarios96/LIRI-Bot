fb3853dc9428eb5e4a4a87efd906ce4d12f5f146
Jordan Larios <jolarios96@gmail.com>
2018-12-07 16:41:55 -0800

    added spotify API call, no data yet

--- liri.js
+++ liri.js
@@ -10,4 +10,31 @@ var fs = require('fs');
 
 var spotify = new Spotify(keys.spotify);
 
+// main app driver
+var query = process.argv[2];
+spotifyData(query);
+
 // functions
+var spotifyData = function (songTitle) {
+    spotify.search({
+        type: 'track',
+        query: song,
+    },
+        function (err, data) {
+            if (err) {
+                return;
+            } else {
+                var songList = data.tracks.items;
+
+                for (var i = 0; i < songList.length; i++){
+                    console.log('Track ' + i + ' of ' + songList.length);
+                    console.log('Album: ');
+                    console.log('Song Title: ');
+                    console.log('Artist(s): ');
+                    console.log('Preview Link: ');
+                    console.log();
+                }
+        }
+        }
+    );
+}
