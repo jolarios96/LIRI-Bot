d3b56ef8ccd18389eba09fb7f4a1c07217c9d746
Jordan Larios <jolarios96@gmail.com>
2018-12-07 16:08:55 -0800

    included node modules and spotify key/secret

--- liri.js
+++ liri.js
@@ -1,4 +1,13 @@
+// dependencies
 require("dotenv").config();
 
+// importing keys and packages
+var keys = require('./keys');
+var Spotify = require('node-spotify-api');
+var axios = require('axios');
+var moment = require('moment');
+var fs = require('fs');
 
 var spotify = new Spotify(keys.spotify);
+
+// functions
