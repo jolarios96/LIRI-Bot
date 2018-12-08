2b0d9c8c85fdd85264c2bcaa188401c691418988
Jordan Larios <jolarios96@gmail.com>
2018-12-07 17:32:57 -0800

    search returns name of artists

--- liri.js
+++ liri.js
@@ -11,27 +11,33 @@ var fs = require('fs');
 var spotify = new Spotify(keys.spotify);
 
 // functions
-var spotifyData = function (songTitle) {
+var getSpotifyData = function (songTitle) {
+    
     spotify.search(
         {
             type: 'track',
-            query: song,
+            query: songTitle,
         },
-        
+
         function (err, data) {
             if (err) {
                 return;
-            } else {
-                var songList = data.tracks.items;
+            }
+            var songList = data.tracks.items;
 
-                for (var i = 0; i < songList.length; i++) {
-                    console.log('Track ' + i + ' of ' + songList.length);
-                    console.log('Album: ');
-                    console.log('Song Title: ');
-                    console.log('Artist(s): ');
-                    console.log('Preview Link: ');
-                    console.log();
-                };
+            for (var i = 0; i < songList.length; i++) {
+            // console.log('Track ' + (i + 1) + ' of ' + songList.length);
+            //     console.log('Album: ');
+
+            // gets name of artist(s) from data
+            console.log('Artists: ' + songList[i].artists.map(function (artist) {
+                return artist.name;
+            }).join(', '));
+            
+            // console.log('Song Title: ' + songlist[i].artists.map(getArtist));
+            //     console.log('Artist(s): ');
+            //     console.log('Preview Link: ');
+            //     console.log();
             };
         }
     );
@@ -40,4 +46,12 @@ var spotifyData = function (songTitle) {
 // main app driver
 var query = process.argv[2];
 var dataInput = process.argv.slice(3).join(' ');
+
+switch (query) {
+    case 'spotify-this-song':
+        getSpotifyData(dataInput);
+        break;
+    case '':
+        break;
+}
 console.log(dataInput);
